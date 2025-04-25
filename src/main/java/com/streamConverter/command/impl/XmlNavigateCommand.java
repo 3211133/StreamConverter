2b6993ef0dda8dc736f0a933934be5485a8cdf1f
3211133 <42115066+3211133@users.noreply.github.com>
2025-04-25 23:18:05 +0900

    create nabigateCommands
    Navigatorクラスの提案

---
+++ src/main/java/com/streamConverter/command/impl/XmlNavigateCommand.java
@@ -0,0 +1,15 @@
+package com.streamConverter.command.impl;
+
+import com.streamConverter.command.AbstractStreamCommand;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class XmlNavigateCommand extends AbstractStreamCommand {
+
+  @Override
+  protected void _execute(InputStream inputStream, OutputStream outputStream) throws IOException {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method '_execute'");
+  }
+}

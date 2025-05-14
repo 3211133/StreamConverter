5d7983adc3487730d56d974fd04be46275a13515
3211133 <42115066+3211133@users.noreply.github.com>
2025-05-14 22:51:59 +0900

    フルパスで変換対象を判定する処理の追加

---
+++ src/main/java/com/streamConverter/pathHandler/IStaXPathHandler.java
@@ -0,0 +1,12 @@
+package com.streamConverter.pathHandler;
+
+import java.util.List;
+
+public interface IStaXPathHandler {
+  /**
+   * 対象階層かどうか判定する。
+   *
+   * @return 対象Xpathで指定した階層かどうか。
+   */
+  boolean isTargetXpath(List<String> xpathList);
+}

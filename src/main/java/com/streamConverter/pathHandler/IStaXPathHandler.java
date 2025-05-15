dc1fc212e2e26fae9c9f3d45d3f648d94d66afe1
3211133 <42115066+3211133@users.noreply.github.com>
2025-05-15 23:18:25 +0900

    Update IStaXPathHandler.java

--- src/main/java/com/streamConverter/pathHandler/IStaXPathHandler.java
+++ src/main/java/com/streamConverter/pathHandler/IStaXPathHandler.java
@@ -2,10 +2,17 @@ package com.streamConverter.pathHandler;
 
 import java.util.List;
 
+/**
+ * Xpathを指定して、Xpathの階層を判定するインターフェース。
+ *
+ * <p>Xpathは、XMLドキュメント内のノードを選択するための言語です。 このインターフェースは、上から読み下すことを想定して、Xpathの機能を一部省略して実装します。
+ */
 public interface IStaXPathHandler {
   /**
    * 対象階層かどうか判定する。
    *
+   * @param xpathList 判定対象のXpathリスト
+   * @throws IllegalArgumentException xpathListがnullの場合
    * @return 対象Xpathで指定した階層かどうか。
    */
   boolean isTarget(List<String> xpathList);

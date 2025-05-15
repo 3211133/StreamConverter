f7ae35aae0de7119d51557f90a2cbf016b69c9d5
3211133 <42115066+3211133@users.noreply.github.com>
2025-05-15 23:18:15 +0900

    update javadocs

--- src/main/java/com/streamConverter/command/impl/XmlNavigateCommand.java
+++ src/main/java/com/streamConverter/command/impl/XmlNavigateCommand.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * XML変換コマンドクラス
+ *
+ * <p>このクラスは、XML形式のデータを変換するためのコマンドを実装します。 ストリームを使用して、XMLデータを読み込み、変換後のデータを出力します。
+ * 変換対象のXPathである箇所を特定したあとに、変換処理を実行することを想定しています。
+ */
 public class XmlNavigateCommand extends AbstractStreamCommand {
 
   @Override

65b21f194c75fcded417dc546d6f8483e186fb18
3211133 <senpanten@gmail.com>
2026-02-26 22:09:47 +0900

    Remove redundant return statement in StreamConverter
    Removed unnecessary return statement from the method.

--- streamconverter-core/src/main/java/com/streamconverter/StreamConverter.java
+++ streamconverter-core/src/main/java/com/streamconverter/StreamConverter.java
@@ -157,8 +157,6 @@ public class StreamConverter {
     if (LOG.isInfoEnabled()) {
       LOG.info("Completed StreamConverter pipeline");
     }
-
-    return results;
   }
 
   /** コマンド（単一または複数）をMDC伝搬付きで並列実行 */
